8ace58d23777d27dc65d6676daf093bdd9bd0425
Tongshi Gong <Tongshi@Tongshis-MacBook-Pro.local>
2018-10-20 01:01:17 -0400

    q2 A

--- predict.R
+++ predict.R
@@ -7,49 +7,126 @@
 
 # Setup -------------------------------------------------------------------
 
-source(library.R)
+source("library.R")
 
-df_clean <- read_csv("sfq_08_16.csv")
+sqf <- read_csv("sqf_08_16.csv")
 
-# Analysis ----------------------------------------------------------------
+# Convert variable types as necessary
+sqf <- sqf %>% mutate(suspect.build = as.factor(suspect.build),
+                      suspect.sex = as.factor(suspect.sex),
+                      location.housing = as.factor(location.housing),
+                      day = as.factor(day),
+                      month = as.factor(month),
+                      time.period = as.factor(time.period),
+                      precinct = as.factor(precinct))
+
+# Part A ----------------------------------------------------------------
 
 # Create training set 
-
-train_set <- df_clean %>% 
+train_set <- sqf %>% 
   filter(
     suspected.crime == "cpw",
     year == "2008"
-  ) %>% 
+  )
+
+# Remember train set standarization values.
+train_age_mean <- mean(train_set$suspect.age, na.rm = TRUE)
+train_age_sd <- sd(train_set$suspect.age, na.rm = TRUE)
+train_height_mean <- mean(train_set$suspect.height, na.rm=TRUE)
+train_height_sd <- sd(train_set$suspect.height, na.rm=TRUE)
+train_weight_mean <- mean(train_set$suspect.weight, na.rm=TRUE)
+train_weight_sd <- sd(train_set$suspect.weight, na.rm=TRUE)
+train_period_mean <- mean(train_set$observation.period, na.rm=TRUE)
+train_period_sd <- sd(train_set$observation.period, na.rm=TRUE)
+
+# Standardize train set.
+train_set <- train_set %>%
   mutate(
-    suspect.age = standardize(suspect.age),
-    suspect.height = standardize(suspect.height),
-    suspect.weight = standardize(suspect.weight),
-    observation.period = standardize(observation.period)
+    suspect.age = (suspect.age - train_age_mean) / train_age_sd,
+    suspect.height = (suspect.height - train_height_mean) / train_height_sd,
+    suspect.weight = (suspect.weight - train_weight_mean) / train_weight_sd,
+    observation.period = (observation.period - train_period_mean) / train_period_sd
   )
 
 # Fit logit regression on training set
-lmodel <- glm(found.weapon ~ precinct + location.housing + addtional.* + stopped.bc.* + suspect.age + suspect.build 
-              + suspect.sex + suspect.height + suspect.weight + inside + radio.run + observation.period +
-                day + month + time.period, data = train_set, family = "binomial")
+train_set <- train_set %>% select(found.weapon, precinct, location.housing, 
+                    contains('additional.'), contains('stopped.bc'), 
+                    suspect.age, suspect.build, suspect.sex, suspect.height, suspect.weight, 
+                    inside, radio.run, observation.period, day, month, time.period)
+lmodel <- glm(found.weapon ~ ., data = train_set, family = "binomial")
 
 # Caculate the probability of a specific instance
-# NEED TO IMPROVE CODES
-friend.prob <- predict(lmodel, suspect.sex = "M",
-                       suspect.age == 30, suspect.height == 6, suspect.weight == 165, 
-                       location.housing == "trainsit", day == "04", month == "10", 
-                       time.period == "6", stopped.bc.bulge == T, additional.proximity == T, 
-                       observation.period == 10, radio.run == F) 
+imaginary_friend <- data.frame(precinct="6", 
+                               location.housing = "transit",
+                               additional.report = F,
+                               additional.investigation = F,
+                               additional.proximity = T,  # close to high crime location?
+                               additional.evasive = F,
+                               additional.associating = F,
+                               additional.direction = F,
+                               additional.highcrime = F,
+                               additional.time = F,
+                               additional.sights = F,
+                               additional.other = F,
+                               stopped.bc.object = F,
+                               stopped.bc.desc = F,
+                               stopped.bc.casing = F,
+                               stopped.bc.lookout = F,
+                               stopped.bc.clothing = F,
+                               stopped.bc.drugs = F,
+                               stopped.bc.furtive = F,
+                               stopped.bc.violent = F,
+                               stopped.bc.bulge = T,
+                               stopped.bc.other = F,
+                               suspect.age = (30 - train_age_mean) / train_age_sd, 
+                               suspect.build = "medium",
+                               suspect.sex = "male",
+                               suspect.height = (6 - train_height_mean) / train_height_sd, 
+                               suspect.weight = (165 - train_weight_mean) / train_weight_sd, 
+                               inside = F,
+                               radio.run = F,
+                               observation.period = (10 - train_period_mean) / train_period_sd,
+                               day = "Thursday", month = "October", 
+                               time.period = "6")
+
+friend.prob.male <- predict(lmodel, newdata=imaginary_friend, type='response') 
+paste0('imaginary friend (male) ex-ante probability of possessing a weapon: ', friend.prob.male)
+
+imaginary_friend_female <- imaginary_friend
+imaginary_friend_female$suspect.sex <- 'female'
+friend.prob.female <- predict(lmodel, newdata=imaginary_friend_female, type='response') 
+paste0('imaginary friend (female) ex-ante probability of possessing a weapon: ', friend.prob.female)
 
 # Compute AUC on 2009 data, using ROCR package
 
-test_09 <- df_clean %>% 
-  filter(year== "2009")
+test_09 <- sqf %>% 
+  filter(year=="2009")
+
+# preprocess as in training data.
+test_09 <- test_09 %>%
+  mutate(
+    suspect.age = (suspect.age - train_age_mean) / train_age_sd,
+    suspect.height = (suspect.height - train_height_mean) / train_height_sd,
+    suspect.weight = (suspect.weight - train_weight_mean) / train_weight_sd,
+    observation.period = (observation.period - train_period_mean) / train_period_sd
+  )
 
 test_09$predicted.probability <- predict(lmodel, newdata = test_09, type = "response")
+test_09 <- test_09 %>% filter(!is.na(predicted.probability))
 
 test_09.pred <- prediction(test_09$predicted.probability, test_09$found.weapon)
 test_09.perf <- performance(test_09.pred, "auc")
-cat("the auc score is", 100*test.perf@y.values[[1]], "\n")
+auc <- 100*test_09.perf@y.values[[1]]
+paste0("the auc score is ", auc)
 
 # Proportion Pairs Comparison
-true_prob <- 
+true_samples <- test_09[test_09$found.weapon == TRUE,]
+true_samples <- true_samples[sample(nrow(true_samples), 10000, replace=T),]
+
+false_samples <- test_09[test_09$found.weapon == FALSE,]
+false_samples <- false_samples[sample(nrow(false_samples), 10000, replace=T),]
+
+approx_auc <- sum(true_samples$predicted.probability > false_samples$predicted.probability) / 10000
+paste0('frequency that a randomly chosen true instance will be ranked higher than a randomly chosen false instance: ', approx_auc)
+
+paste0('Confirmed: ', auc, ' is close to ', approx_auc)
